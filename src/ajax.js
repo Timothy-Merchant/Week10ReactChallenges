ac8b18a3b8c100939e441e55aae8eec41ba4bde9
Tim <ryalin1286@gmail.com>
2021-03-09 12:23:26 +0000

    completed ajax challenge 03

--- src/ajax.js
+++ src/ajax.js
@@ -4,13 +4,18 @@ import axios from "./axios";
 // ...challenges here
 
 // 1
-axios.get("/articles").then(({ data }) => {
-    console.log(data);
-});
+// axios.get("/articles").then(({ data }) => {
+//     console.log(data);
+// });
 
 // 2
-axios.post("/articles", {
-    title: "Ajax challenge 02",
-    content: "now we're using ajax",
-    tags: ["A", "J", "A", "X"]
-});
+// axios.post("/articles", {
+//     title: "Ajax challenge 02",
+//     content: "now we're using ajax",
+//     tags: ["A", "J", "A", "X"]
+// });
+
+// 3
+axios.get("/articles/15").then(({data}) => {
+    console.log(data);
+})

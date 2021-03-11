192d0b3baead9dba0a979b92ae64ffbc6a380f18
Tim <ryalin1286@gmail.com>
2021-03-11 16:01:59 +0000

    completed reducer testing challenge 4

--- src/ToDo/reducer.test.js
+++ src/ToDo/reducer.test.js
@@ -15,6 +15,15 @@ it('adds items', () => {
     // first item in the items array should be a new task with "Hello"
     expect(result.items[0]).toEqual({ task: "Hello", completed: false });
     expect(result.items).not.toBe(initialState.items);
+
+    // passing in the previous result, which already had one item
+    result = addItem(result, { value: "Mum" });
+    // should be two items
+    expect(result.items.length).toBe(2);
+
+    // check both items are now in items, in order given
+    expect(result.items[0]).toEqual({ task: "Hello", completed: false });
+    expect(result.items[1]).toEqual({ task: "Mum", completed: false });
 });
 
 // it('removes items', () => {
